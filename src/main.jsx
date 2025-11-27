f96c84568c07f22b01918176bdcdc45bb3f4d2c6
VictorG22 <victorgaona702@gmail.com>
2025-11-27 14:22:20 -0800

    reverting and setting up for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <Router basename='/filmz'>
+  <Router>
     <App />
   </Router>,
 )
